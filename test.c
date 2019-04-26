7119fb8ae8be33240641f61ab7b133e592092a78
NachoYo <49642496+NachoYo@users.noreply.github.com>
2019-04-26 18:50:47 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-
-void main(int argc, char *argv[])
-{
-	printf("Argc %i \nArgv1 %c\n Argv2 %c\nArgv3 %c\n",argc,argv[0],argv[1],argv[2]);
-}
-#include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
