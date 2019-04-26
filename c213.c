42d968705cb536fed08fb32ac6483366129c9899
israelo96 <49364851+israelo96@users.noreply.github.com>
2019-04-26 22:56:34 +0900

    Update c213.c

--- c213.c
+++ c213.c
@@ -172,9 +172,10 @@ static void * handle(void * arg)
 		for(int i=0;i<5;i++)
 		{
 		table[atoi(recv_buffer[1])][i]=atoi(recv_buffer[i+3]);
-		}
 		if(recv_buffer[1]=='1'&&i==4)
 			begin=true;
+		}
+		
 	}
 	close(cli_sockfd);
 	ret = 0;
