8d79f4245ca4d4f8cc382b07ee9b87278d7a14e0
MarcoHdzP <50066703+MarcoHdzP@users.noreply.github.com>
2019-04-28 17:58:33 +0900

    Update c213.c

--- c213.c
+++ c213.c
@@ -9,8 +9,8 @@
 
 char *buffer;
 char r_buffer[1024];
-char *costs="# 1 3 4 5 6 7";
-int table[5][5]={{0,1,3,1,1},
+char *costs="# 2 2 9 0 0 9";
+int table[5][5]={{0,0,0,0,0},
  {0,0,0,0,0},
  {0,0,0,0,0},
 {0,0,0,0,0}};
