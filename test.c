037fae8fbbd5bdc0d0f3ac86020a6a76eb4b32e8
NachoYo <49642496+NachoYo@users.noreply.github.com>
2019-04-29 00:38:38 +0900

    Update test.c

--- test.c
+++ test.c
@@ -198,10 +198,9 @@ int main(int argc , char *argv[])
 				 sleep(1);
 				 for(int i=0;i<sizeof(client_socket);i++)
 				 {
-					 sleep(1);
+					send(client_socket[i],message2,strlen(message2),0); 
 					 message2="+0123110980290093800710970";
 					send(client_socket[i],message2,strlen(message2),0); 
-					 printf("Mandado a la pc:%d\n",i);
 				  }
 				 printf("Salio del For\n");
 				 /*
