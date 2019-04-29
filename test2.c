843a80c23b8c55e6a29c1f0712b780061a5bc73a
NachoYo <49642496+NachoYo@users.noreply.github.com>
2019-04-29 23:05:11 +0900

    Update test2.c

--- test2.c
+++ test2.c
@@ -207,7 +207,8 @@ int main(int argc , char *argv[])
 				 for(int i=0;i<sizeof(client_socket);i++)
 				 {
 					send(client_socket[i],message2,strlen(message2),0); 
-					 message2="+0123110980290093800710970";
+					 message2=(char *)mess_buff;
+					 //message2="+0123110980290093800710970";
 					send(client_socket[i],message2,strlen(message2),0); 
 				  }
 				 printf("Salio del For\nTabla concac:%s\n",mess_buff);
