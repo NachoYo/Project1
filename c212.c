f754d4f0e10c637d0d072e26553970b47e9d4676
NachoYo <49642496+NachoYo@users.noreply.github.com>
2019-04-30 00:51:31 +0900

    Update c212.c

--- c212.c
+++ c212.c
@@ -52,6 +52,52 @@ int main()
 		return 0;
 	}
 	while (1) {
+		memset(r_buffer, 0, sizeof(r_buffer));
+		len = recv(fd_sock, r_buffer, sizeof(r_buffer), 0);
+		if(r_buffer[0]=='+'){
+			for(int e=0;e<5;e++){
+				for(int j=0;j<5;j++){
+				sprintf(auxiliar,"%c",r_buffer[e*5+j+1]);
+				table[e][j]=atoi(auxiliar);
+				printf("Elemento q guarda:%c\n elemento guardado:%d\n",r_buffer[e*5+j+1],table[e][j]);
+					memset(auxiliar, 0, sizeof(auxiliar));
+				}
+				printf("Cambio de linea %d\n",e);
+			}
+			printf("TABLE[1] %d %d %d %d %d \n",table[0][0],table[0][1],table[0][2],table[0][3],table[0][4]);
+			 printf("TABLE[2] %d %d %d %d %d \n",table[1][0],table[1][1],table[1][2],table[1][3],table[1][4]);
+			 printf("TABLE[3] %d %d %d %d %d \n",table[2][0],table[2][1],table[2][2],table[2][3],table[2][4]);
+			 printf("TABLE[4] %d %d %d %d %d \n",table[3][0],table[3][1],table[3][2],table[3][3],table[3][4]);
+			printf("TABLE[5] %d %d %d %d %d \n",table[4][0],table[4][1],table[4][2],table[4][3],table[4][4]);
+			memset(r_buffer, 0, sizeof(r_buffer));
+		}
+		else if(r_buffer[0]!='1'&&r_buffer[1]=='2')
+		{
+			printf("(Forwarded from computer no.1)\nComputer no.%c says: ",r_buffer[0]);
+			for(int i=2;i<sizeof(r_buffer);i++){
+			printf("%c",r_buffer[i]);
+			}
+		}
+		else if(r_buffer[0]=='1'&&r_buffer[1]=='2')
+		{
+			printf("(Sent directly)\nComputer no.%c says: ",r_buffer[0]);
+			for(int i=2;i<sizeof(r_buffer);i++){
+			printf("%c",r_buffer[i]);
+			}
+		}
+		fflush(NULL);
+		buffer = NULL;
+		}
+	}
+	// bye-bye
+	close(fd_sock);
+	return 0;
+}
+
+static void * listenmsg(void * arg)
+{
+	while(1){
+		
 		if(begin){
 		//sending
 		printf("Which machine do you want to send a message?\n");
@@ -91,51 +137,8 @@ int main()
 			fflush(NULL);
 			buffer = NULL;
 			pthread_create(&listenthd, NULL, listenmsg, (void*)&len);
-		}
-	}
-	// bye-bye
-	close(fd_sock);
-	return 0;
-}
-
-static void * listenmsg(void * arg)
-{
-	while(1){
-		memset(r_buffer, 0, sizeof(r_buffer));
-		len = recv(fd_sock, r_buffer, sizeof(r_buffer), 0);
-		if(r_buffer[0]=='+'){
-			for(int e=0;e<5;e++){
-				for(int j=0;j<5;j++){
-				sprintf(auxiliar,"%c",r_buffer[e*5+j+1]);
-				table[e][j]=atoi(auxiliar);
-				printf("Elemento q guarda:%c\n elemento guardado:%d\n",r_buffer[e*5+j+1],table[e][j]);
-					memset(auxiliar, 0, sizeof(auxiliar));
-				}
-				printf("Cambio de linea %d\n",e);
-			}
-			printf("TABLE[1] %d %d %d %d %d \n",table[0][0],table[0][1],table[0][2],table[0][3],table[0][4]);
-			 printf("TABLE[2] %d %d %d %d %d \n",table[1][0],table[1][1],table[1][2],table[1][3],table[1][4]);
-			 printf("TABLE[3] %d %d %d %d %d \n",table[2][0],table[2][1],table[2][2],table[2][3],table[2][4]);
-			 printf("TABLE[4] %d %d %d %d %d \n",table[3][0],table[3][1],table[3][2],table[3][3],table[3][4]);
-			printf("TABLE[5] %d %d %d %d %d \n",table[4][0],table[4][1],table[4][2],table[4][3],table[4][4]);
-			memset(r_buffer, 0, sizeof(r_buffer));
-		}
-		else if(r_buffer[0]!='1'&&r_buffer[1]=='2')
-		{
-			printf("(Forwarded from computer no.1)\nComputer no.%c says: ",r_buffer[0]);
-			for(int i=2;i<sizeof(r_buffer);i++){
-			printf("%c",r_buffer[i]);
-			}
-		}
-		else if(r_buffer[0]=='1'&&r_buffer[1]=='2')
-		{
-			printf("(Sent directly)\nComputer no.%c says: ",r_buffer[0]);
-			for(int i=2;i<sizeof(r_buffer);i++){
-			printf("%c",r_buffer[i]);
-			}
-		}
-		fflush(NULL);
-		buffer = NULL;
+		
+		
 	}
 }
 
