328c9f7fe6429c5b3bd26ba8962101bb7feb9d5a
israelo96 <49364851+israelo96@users.noreply.github.com>
2019-04-30 02:39:16 +0900

    Update c215.c

--- c215.c
+++ c215.c
@@ -7,7 +7,9 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#define INFINITY 9999
 
+char *addrs[] = {"220.149.244.211", "220.149.244.212", "220.149.244.213","220.149.244.214","220.149.244.215"};
 int state1=0, state2=0;
 char *buffer;
 char r_buffer[1024];
@@ -66,12 +68,13 @@ int main()
 				}
 				printf("Cambio de linea %d\n",e);
 			}
-			printf("TABLE[1] %d %d %d %d %d \n",table[0][0],table[0][1],table[0][2],table[0][3],table[0][4]);
+			/*printf("TABLE[1] %d %d %d %d %d \n",table[0][0],table[0][1],table[0][2],table[0][3],table[0][4]);
 			 printf("TABLE[2] %d %d %d %d %d \n",table[1][0],table[1][1],table[1][2],table[1][3],table[1][4]);
 			 printf("TABLE[3] %d %d %d %d %d \n",table[2][0],table[2][1],table[2][2],table[2][3],table[2][4]);
 			 printf("TABLE[4] %d %d %d %d %d \n",table[3][0],table[3][1],table[3][2],table[3][3],table[3][4]);
-			printf("TABLE[5] %d %d %d %d %d \n",table[4][0],table[4][1],table[4][2],table[4][3],table[4][4]);
+			printf("TABLE[5] %d %d %d %d %d \n",table[4][0],table[4][1],table[4][2],table[4][3],table[4][4]);*/
 			memset(r_buffer, 0, sizeof(r_buffer));
+			dijkstra(table,5,4);
 			if(state1==1)
 				printf("Which machine do you want to send a message?\n");
 			else if(state2==1)
@@ -158,25 +161,25 @@ static void * listenmsg(void * arg)
 		}
 	}
 }
-
-void dijkstra(int G[5][5],int startnode)
+void dijkstra(int G[5][5],int n,int startnode)
 {
  
 	int cost[5][5],distance[5],pred[5];
 	int visited[5],count,mindistance,nextnode,i,j;
+	int cnt=1;
 	
 	//pred[] stores the predecessor of each node
 	//count gives the number of nodes seen so far
 	//create the cost matrix
-	for(i=0;i<5;i++)
-		for(j=0;j<5;j++)
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
 			if(G[i][j]==0)
 				cost[i][j]=INFINITY;
 			else
 				cost[i][j]=G[i][j];
 	
 	//initialize pred[],distance[] and visited[]
-	for(i=0;i<5;i++)
+	for(i=0;i<n;i++)
 	{
 		distance[i]=cost[startnode][i];
 		pred[i]=startnode;
@@ -187,12 +190,12 @@ void dijkstra(int G[5][5],int startnode)
 	visited[startnode]=1;
 	count=1;
 	
-	while(count<5-1)
+	while(count<n-1)
 	{
 		mindistance=INFINITY;
 		
 		//nextnode gives the node at minimum distance
-		for(i=0;i<5;i++)
+		for(i=0;i<n;i++)
 			if(distance[i]<mindistance&&!visited[i])
 			{
 				mindistance=distance[i];
@@ -201,7 +204,7 @@ void dijkstra(int G[5][5],int startnode)
 			
 			//check if a better path exists through nextnode			
 			visited[nextnode]=1;
-			for(i=0;i<5;i++)
+			for(i=0;i<n;i++)
 				if(!visited[i])
 					if(mindistance+cost[nextnode][i]<distance[i])
 					{
@@ -212,18 +215,21 @@ void dijkstra(int G[5][5],int startnode)
 	}
  
 	//print the path and distance of each node
-	for(i=0;i<5;i++)
+	for(i=0;i<n;i++)
 		if(i!=startnode)
 		{
-			printf("\nDistance of node%d=%d",i,distance[i]);
-			printf("\nPath=%d",i);
+			printf("Routing Table - Computer %d (%s)",startnode+1,addrs[startnode]);
+			//printf("\nPath=%d",i);
 			
 			j=i;
 			do
 			{
+				cnt++;
 				j=pred[j];
-				printf("<-%d",j);
+				//printf("<-%d",j);
 			}while(j!=startnode);
+			printf("Destination Computer: %d (%s) No. of hops: %d Total distnce: %d\n",i+1,addrs[i], cnt-1, distance[i]);
+			cnt=1;
 	}
 }
 
