7d789891a89e6b640130bae82c1dea190e6fbf64
MarcoHdzP <50066703+MarcoHdzP@users.noreply.github.com>
2019-04-30 16:54:38 +0900

    Update c214.c

--- c214.c
+++ c214.c
@@ -57,6 +57,7 @@ int main()
 	}
 	pthread_create(&listenthd, NULL, listenmsg, (void*)&len);
 	while (1) {
+		printf("LE LLEGO ESTO: %s\n",r_buffer);
 		len = recv(fd_sock, r_buffer, sizeof(r_buffer), 0);
 		if(r_buffer[0]=='+'){
 			for(int e=0;e<5;e++){
@@ -90,8 +91,7 @@ int main()
 			if(state1==1)
 				printf("Which machine do you want to send a message?\n");
 			else if(state2==1)
-				printf("Type your message (for the machine you typed):\n");
-				
+				printf("Type your message (for the machine you typed):\n");	
 		}
 		else if(r_buffer[0]=='1'&&r_buffer[1]=='4')
 		{
@@ -173,7 +173,7 @@ static void * listenmsg(void * arg)
 			 	printf("TABLE[4] %d %d %d %d %d \n",table[3][0],table[3][1],table[3][2],table[3][3],table[3][4]);
 				printf("TABLE[5] %d %d %d %d %d \n",table[4][0],table[4][1],table[4][2],table[4][3],table[4][4]);
 				memset(r_buffer, 0, sizeof(r_buffer));
-				dijkstra(table,5,3);
+				dijkstra(table,5,1);
 				printf("\n-----------------------\n\n");
 				if(state1==1){
 					printf("Now you can send messages to any computer! (1 to 5)\n");
